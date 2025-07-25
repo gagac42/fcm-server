c44b838a2002a8884375c1a87c1492d62c08da74
gagac42 <gagac42@gmail.com>
2025-07-25 15:20:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/send', async (req, res) => {
     const auth = new GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+       private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
       },
       scopes: ['https://www.googleapis.com/auth/firebase.messaging']
     });
